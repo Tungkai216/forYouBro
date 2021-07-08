d5ef61ffdb0ca4257339b360eb75d6f080790e15
Tungkai216 <michaelkrsale@gmail.com>
2021-07-08 15:06:47 +0700

    Mengubah css,Menambahkan pesan di html, dan mengaktifkan notive.js

--- js/notive.js
+++ js/notive.js
@@ -1,21 +1,21 @@
 
-// var lagi = true
-// var alerted = localStorage.getItem('alerted') || '';
-//     if (alerted != 'f') {
+var lagi = true
+var alerted = localStorage.getItem('alerted') || '';
+    if (alerted != 'f') {
 
-//      while (lagi === true) {
-//   var nama = prompt('Tak kenal maka tak sayang\n(Isi nama ishh)');
-//   alert('Halo ' +nama +' salam kenal');
-//   alert('Lama tak berjumpa ya' + ' '+ nama);
-//   alert('Cuman mau beritahu aja ' +nama);
-//   alert('Untuk full screen tinggal tekan f11');
-//   alert('Ok itu aja informasinya, selebihnya terimakasih '+ nama);
-//   lagi = confirm('Mau baca informasinya lagi?');
+     while (lagi === true) {
+  var nama = prompt('Tak kenal maka tak sayang\n(Isi nama ishh)');
+  alert('Halo ' +nama +' salam kenal');
+  alert('Lama tak berjumpa ya' + ' '+ nama);
+  alert('Cuman mau beritahu aja ' +nama);
+  alert('Untuk full screen tinggal tekan f11');
+  alert('Ok itu aja informasinya, selebihnya terimakasih '+ nama);
+  lagi = confirm('Mau baca informasinya lagi?');
 
-// }
-// alert('Enjoy this web ' + nama);
-// alert('And one thing miss you xrvfun');
+}
+alert('Enjoy this web ' + nama);
+alert('And one thing miss you xrvfun');
 
-//      localStorage.setItem('alerted','f');
-//     }
+     localStorage.setItem('alerted','f');
+    }
 
